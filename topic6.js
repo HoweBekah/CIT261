b026ff6ed7a87610a49afb856c6c0c31c557824d
HoweBekah <how18019@byui.edu>
2019-02-16 20:01:12 -0700

    js addition and json

---
+++ topic6.js
@@ -0,0 +1,40 @@
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+
+var requestURL = 'https://howebekah.github.io/CIT261/json/villain.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+
+request.onload = function () {
+    var afiText = request.response;
+    var afimovies = JSON.parse(afiText);
+    populateHeader(afimovies);
+    showHeroes(afimovies);
+}
+
+function showMovies(jsonObj) {
+    var movies = jsonObj['movies'];
+
+    for (var i = 0; i < movies.length; i++) {
+        var myArticle = document.createElement('article');
+        var myH4 = document.createElement('h4');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+
+
+        myH4.textContent = movies[i].title;
+        myPara1.textContent = 'Rating: ' + movies[i].rating;
+        myPara2.textContent = 'Director: ' + movies[i].director;
+        myPara3.textContent = 'Year: ' + movies[i].year;
+
+        myArticle.appendChild(myH4);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+
+        section.appendChild(myArticle);
+    }
+}
