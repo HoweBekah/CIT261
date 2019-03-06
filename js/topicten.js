675363acfda43760a81c1424e45bf1b384bbac27
HoweBekah <how18019@byui.edu>
2019-03-05 20:41:58 -0700

    finish topic 10

--- js/topicten.js
+++ js/topicten.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("ogDrawing");
-    ctx.fillStyle = "rgb(98, 114, 255)";
-    ctx.drawImage(img, 25, 25);
+    var canstuff = canvas.getContext("2d");
+    var drawing = document.getElementById("ogDrawing");
+    canstuff.fillStyle = "#612A03";
+    canstuff.drawImage(drawing, 25, 25);
 };
