70f799a1404c149f022508b5202f68a6fbafae5a
HoweBekah <how18019@byui.edu>
2019-04-07 00:44:19 -0700

    most of mad libs coded

--- MadWords/js/words.js
+++ MadWords/js/words.js
@@ -1,54 +1,45 @@
-var section = document.querySelector("section");
-
+var fillin = document.getElementById("fillWords");
+var libbed = document.getElementById("madlib");
 var requestURL =
-    "http://madlibz.herokuapp.com/api/random?minlength=2&maxlength=7";
+    "https://next.json-generator.com/api/json/get/Ek0JxcMYI";
 var request = new XMLHttpRequest();
-request.open("GET", requestURL);
+request.open("GET", requestURL, true);
 request.responseType = "text";
 request.send();
 
 request.onload = function () {
     var madText = request.responseText;
     var libwords = JSON.parse(madText);
-    var libs = libwords["templates"];
     var ran = Math.floor(Math.random() * Math.floor(5));
-    var picked = libs[ran];
+    libwords = libwords["templates"];
+    libwords = libwords[ran];
+    console.log(libwords);
     var h2 = document.createElement("h2");
-    h2.textContent = picked.title;
-    popLib(picked);
+    h2.textContent = libwords.title;
+    var art = document.createElement("article");
+    fillin.appendChild(h2);
+    for (var i = 0; i < libwords.story.length - 1; i++) {
+        var input = document.createElement("input");
+        var label = document.createElement("label");
+        var brake = document.createElement("br");
+        var para = document.createElement("p");
+        input.type = "text";
+        var wordtype = libwords.blanks[i];
+        input.id = "word" + i;
+        neededId = "word" + i;
+        label.textContent = wordtype + ": ";
+        label.setAttribute("for", input);
+        fillin.insertBefore(label, document.getElementById("input"));
+        fillin.appendChild(input);
+        fillin.appendChild(brake);
+        var neededId = "word" + i;
+        para.textContent = libwords.story[i] + neededId.value;
+        art.appendChild(para);
+    }
+
+    libbed.appendChild(art);
 };
 
-function popLib(madwords) {
-
-
-    var para1C = document.createElement("p");
-    var para2C = document.createElement("p");
-    var para3C = document.createElement("p");
-    var para4C = document.createElement("p");
-    for (var i = 0; i < madwords.blanks.length; i++) {
-
-
-        h3C.textContent = towns[i].name;
-        para1C.textContent = '"' + towns[i].motto + '"';
-        para2C.textContent = "Year Founded: " + towns[i].yearFounded;
-        para3C.textContent = "Current Population: " + towns[i].currentPopulation;
-        para4C.textContent = "Average Rainfall: " + towns[i].averageRainfall;
-        para1C.style.fontWeight = "bolder";
-
-        articleC.appendChild(h3C);
-        articleC.appendChild(para1C);
-        articleC.appendChild(para2C);
-        articleC.appendChild(para3C);
-        articleC.appendChild(para4C);
-
-
-        for (var j = i; j < jPics.length;) {
-            var pic = document.createElement("img");
-            pic.src = jPics[j];
-            articleC.appendChild(pic);
-            break;
-        }
-
-        section.appendChild(articleC);
-    }
+function popLib() {
+    libbed.className = "seeit";
 }
