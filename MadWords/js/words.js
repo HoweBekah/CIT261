530e0c96b863cafc0de475a33a8c3b38c4618c8a
HoweBekah <how18019@byui.edu>
2019-04-08 14:46:48 -0700

    mad libs done basically

--- MadWords/js/words.js
+++ MadWords/js/words.js
@@ -6,40 +6,48 @@ var request = new XMLHttpRequest();
 request.open("GET", requestURL, true);
 request.responseType = "text";
 request.send();
+var madText;
+var libwords;
+var ran;
 
 request.onload = function () {
-    var madText = request.responseText;
-    var libwords = JSON.parse(madText);
-    var ran = Math.floor(Math.random() * Math.floor(5));
+    madText = request.responseText;
+    libwords = JSON.parse(madText);
+    ran = Math.floor(Math.random() * Math.floor(5));
     libwords = libwords["templates"];
     libwords = libwords[ran];
-    console.log(libwords);
+
     var h2 = document.createElement("h2");
     h2.textContent = libwords.title;
-    var art = document.createElement("article");
     fillin.appendChild(h2);
+    //can't change this to blanks so that there isn't an extra undefined input or else the popLib() doesn't work
     for (var i = 0; i < libwords.story.length - 1; i++) {
         var input = document.createElement("input");
         var label = document.createElement("label");
         var brake = document.createElement("br");
-        var para = document.createElement("p");
+
         input.type = "text";
         var wordtype = libwords.blanks[i];
         input.id = "word" + i;
-        neededId = "word" + i;
         label.textContent = wordtype + ": ";
         label.setAttribute("for", input);
         fillin.insertBefore(label, document.getElementById("input"));
         fillin.appendChild(input);
         fillin.appendChild(brake);
-        var neededId = "word" + i;
-        para.textContent = libwords.story[i] + neededId.value;
-        art.appendChild(para);
     }
 
-    libbed.appendChild(art);
 };
 
 function popLib() {
-    libbed.className = "seeit";
+    var para = document.createElement("p");
+    para.textContent += "\"";
+    for (var i = 0; i < libwords.story.length - 1; i++) {
+        para.textContent += libwords.story[i] + document.getElementById("word" + i).value;
+    }
+    para.id = "lib";
+    console.log(para);
+    para.textContent += "\"";
+    libbed.appendChild(para);
+    //libbed.className = "seeit";
+
 }
